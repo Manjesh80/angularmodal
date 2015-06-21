d10d370f9ca68afa0136f03139fe998112d122fb
Manjesh Gowda <manjesh.2j@gmail.com>
2015-06-20 21:09:54 -0400

    Angular Modal Options

---
+++ app/AngularFormsApp.js
@@ -0,0 +1,55 @@
+﻿
+var angularFormsApp = angular.module('angularFormsApp', ['ngRoute', 'ui.bootstrap']);
+
+angularFormsApp.config(function ($routeProvider) {
+    $routeProvider
+        .when("/home", {
+            templateUrl: "app/Home.html",
+            controller: "HomeController"
+        })
+        .when("/newEmployeeForm", {
+            templateUrl: "app/EmployeeForm/efTemplate.html",
+            controller: "efController"
+        })
+        .when("/updateEmployeeForm/:id", {
+            templateUrl: "app/EmployeeForm/efTemplate.html",
+            controller: "efController"
+        })
+        .otherwise({
+            redirectTo: "/home"
+        });
+});
+
+angularFormsApp.controller("HomeController",
+    function ($scope, $location, $modal, DataService) {
+
+        $scope.showCreateEmployeeForm = function () {
+            //$location.path('/newEmployeeForm');
+
+            $modal.open({
+                templateUrl: 'app/EmployeeForm/efTemplate.html', 
+                controller: 'efController' 
+            });
+
+        };
+
+        $scope.showUpdateEmployeeForm = function (id) {
+            //$location.path('/updateEmployeeForm/' + id
+            //var modelScope = $scope.$new(true);
+
+            //modelScope.empID = id;
+            
+            $modal.open({
+                templateUrl: 'app/EmployeeForm/efTemplate.html',
+                controller: 'efController',
+                //scope: modelScope,
+                resolve: {
+                    employee: function () {
+                        return DataService.getEmployee(id);
+                    }
+                }
+            });
+        };
+
+    });
+
---
+++ app/EmployeeForm/efController.js
@@ -0,0 +1,46 @@
+﻿
+angularFormsApp.controller('efController',
+    function efController($scope, $window, $routeParams, $modalInstance, DataService, employee) {
+
+        //alert($scope.empID);
+
+        //if ($scope.empID)
+        //    $scope.employee = DataService.getEmployee($scope.empID);
+        //else
+        //    $scope.employee = { id: 0 };
+
+        $scope.employee = employee || { id: 0 };
+
+        $scope.editableEmployee = angular.copy($scope.employee);
+
+        $scope.departments = [
+            "Engineering",
+            "Marketing",
+            "Finance",
+            "Administration"
+        ];
+
+        $scope.submitForm = function () {
+
+            if ($scope.editableEmployee.id == 0) {
+                // insert new employee
+                DataService.insertEmployee($scope.editableEmployee);
+            }
+            else {
+                // update the employee
+                DataService.updateEmployee($scope.editableEmployee);
+            }
+
+            $scope.employee = angular.copy($scope.editableEmployee);
+            //$window.history.back();
+
+            $modalInstance.close();
+        };
+
+        $scope.cancelForm = function () {
+            //$window.history.back();
+
+            $modalInstance.dismiss();
+        };
+
+    });
